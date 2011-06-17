6e29c5cb4400f07652babc43b2c26cfb5aac97ba
James Rowe <jnrowe@gmail.com>
2011-06-17 12:18:53 +0100

    Added tea timer script.

---
+++ tea.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+# Lazy?  Forgetful?  Love tea?  inject the lazy forgetfulness from here.
+
+source $(dirname $(readlink -f ${0}))/tool_settings.sh
+
+brew() {
+    dzen2 -fg ${FOREGROUND} -bg ${BACKGROUND} -fn ${FONT} -p ${TIMEOUT} \
+        <<< "Brewing tea - Sit back and relax"
+    sleep ${1}m
+    dzen2 -fg ${FOREGROUND} -bg ${BACKGROUND} -fn ${FONT} -p ${TIMEOUT} \
+        <<< "Tea ready - Go and get it"
+}
+
+case "${1}" in
+red)
+    brew 5 ;;
+green)
+    brew 4 ;;
+ginger)
+    brew 3.5 ;;
+*)
+    for i in {1..3}; do echo -e '\007'; sleep 0.25s; done &
+    dzen2 -fg ${FOREGROUND} -bg ${BACKGROUND} -fn ${FONT} -p ${TIMEOUT} \
+        <<< "tea.sh - ^fg(#ff0000)Unknown tea type"
+    exit 1
+esac
